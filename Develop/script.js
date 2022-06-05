e99bfc3cd221e03aab50f66f042f69b9b7810f6d
gilllo <g@zora.co>
2022-06-06 08:30:08 +1000

    added input for highscores

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,10 @@
 var startButton = document.querySelector(".startButton");
-var timerElement = document.querySelector(".time")
-var q1Element = document.querySelector(".q1")
-var q2Element = document.querySelector(".q2")
-var q3Element = document.querySelector(".q3")
-var q4Element = document.querySelector(".q4")
-var q5Element = document.querySelector(".q5")
+var timerElement = document.querySelector("#countdown");
+var q1Element = document.querySelector(".q1");
+var q2Element = document.querySelector(".q2");
+var q3Element = document.querySelector(".q3");
+var q4Element = document.querySelector(".q4");
+var q5Element = document.querySelector(".q5");
 
 var isCorrect = false;
 var timer;
@@ -21,7 +21,7 @@ function startQuiz() {
 // NEED TO ADD REDUCTION IN TIME FOR INCORRECT ANSWER
 function startTimer() {
     timer = setInterval(function() {
-        timerCount --;
+        timerCount--;
         timerElement.textContent = timerCount;
         if (timerCount >= 0) {
             if (isCorrect && timerCount > 0) {
@@ -41,19 +41,19 @@ function loseQuiz() {
 
 
 //      write global variables
-    // questions
-    // start button
-    // options to choose from in questions 
-    // timer 
+// questions
+// start button
+// options to choose from in questions 
+// timer 
 
 // 1. add event listener to start quiz button which starts function to start quiz
-    // start quiz reveals question 1 and starts timer down from 75 secs
-    // write interval timer function which takes 10 seconds each wrong answer
+// start quiz reveals question 1 and starts timer down from 75 secs
+// write interval timer function which takes 10 seconds each wrong answer
 
-    // start button shows question 1 
+// start button shows question 1 
 
 // 2. quiz function
-    // if/else function for answers to select correct answer 
-    // show either "correct" or "incorrect" after seelction is made 
+// if/else function for answers to select correct answer 
+// show either "correct" or "incorrect" after seelction is made 
 // 3. after quiz input for initials and log score in local storage
 // view highscore function to show previous scores when logged
