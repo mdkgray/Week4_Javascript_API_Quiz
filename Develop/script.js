c719bffc017afc031987dbf853e30d4b467dd306
Mackenzie Gray <mdkulen@gmail.com>
2022-06-07 09:54:19 +1000

    functions for start quiz and cycle questions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // variables for start button
 var startButton = document.querySelector(".startButton");
 
+// variable for div with landing page
+var landingPage = document.querySelector(".start");
+
 // variables for timer 
 var timerElement = document.querySelector("#countdown");
 var score = document.querySelector("#scoreInput");
@@ -12,9 +15,9 @@ var timerCount;
 // variables for questions section
 var allQuestionsEl = document.querySelector(".all-question");
 
-// variables for question elements outside of question ul
+// variables for question elements and right/wrong
 var questionEl = document.querySelector("#question");
-var correctWrong = document.querySelector("#right-wrong");
+var rightWrong = document.querySelector("#right-wrong");
 var questionCount = 0;
 
 // variable for final score
@@ -42,7 +45,7 @@ var ans3Btn = document.querySelector("#answer-3");
 var ans4Btn = document.querySelector("#answer-4");
 
 // questions array (index starts at [0])
-const questions = [
+var questions = [
     {
         question: "Commonly used data types DO NOT include:",
         answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
@@ -70,6 +73,7 @@ const questions = [
     }
 ];
 
+// start timer function 
 function startTimer() {
     timer = setInterval(function() {
         timerCount--;
@@ -88,10 +92,37 @@ function startTimer() {
     }, 1000);
 }
 
+// start quiz function 
 function startQuiz() {
+    landingPage.style.display = "none";
+    allQuestionsEl.style.display = "block";
+    questionCount = 0;
     timerCount = 75;
     startButton.disabled = true;
+    
     startTimer();
+    cycleQuestion(questionCount);
+}
+
+// cycle question function 
+function cycleQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
+
+// checking answers function 
+function checkAnswers(event) {
+    event.preventDefault();
+
+    // display right/wrong <p> tag underneath question options 
+    rightWrong.style.display = "block";
+    var questionStatus = document.createElement("p");
+    rightWrong.appendChild(questionStatus);
 }
 
 
