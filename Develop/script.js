ace719a9cc29e6a9014741ae7a92c4548650774f
Mackenzie Gray <mdkulen@gmail.com>
2022-06-06 10:16:20 +1000

    updated html and css

--- Develop/script.js
+++ Develop/script.js
@@ -57,3 +57,42 @@ function loseQuiz() {
 // show either "correct" or "incorrect" after seelction is made 
 // 3. after quiz input for initials and log score in local storage
 // view highscore function to show previous scores when logged
+// function to clear highscores from list 
+
+
+
+
+
+        //   "q1"
+        //     Commonly used data types DO NOT include:
+        //     1. strings
+        //     2. booleans
+        //     3. alerts
+        //     4. numbers
+        
+        // "q2"
+        //     <The condition in an if/else statement is enclosed within _____.</
+        //     1. quotes
+        //     2. curly brackets
+        //     3. parentheses
+        //     4. square brackets
+        
+        //  q3
+        //     <Arrays in JavaScript can be used to store _____.</
+        //     1. numbers and strings
+        //     2. other arrays
+        //     3. booleans
+        //     4. all of the above
+        //  q4
+        //    <String values must be enclosed within _____ when being assigned to variables.</
+        //     1. commas
+        //     2. curly brackets
+        //     3. quotes
+        //     4. parentheses
+        //  q5
+        //     A very useful tool used during development and debugging for printing content to the debugger is:
+        //     1. JavaScript
+        //     2. terminal/bash
+        //     3. for loops
+        //     4. console.log
+        
